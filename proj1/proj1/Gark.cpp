96382772f33f5f2ed8f09c19f92991cce3ad65a6
lzhou0714 <lzhou08714@g.ucla.edu>
2022-01-06 12:12:00 -0800

    player and gark headers

---
+++ proj1/proj1/Gark.cpp
@@ -0,0 +1,8 @@
+//
+//  Gark.cpp
+//  proj1
+//
+//  Created by Lily Zhou on 1/6/22.
+//
+
+//#include "Gark.h"
---
+++ proj1/proj1/Gark.h
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class Mesa;
+class Gark
+{
+  public:
+        // Constructor
+    Gark(Mesa* mp, int r, int c);
+
+        // Accessors
+    int  row() const;
+    int  col() const;
+
+        // Mutators
+    void move();
+    bool getAttacked(int dir);
+
+  private:
+    Mesa* m_mesa;
+    int   m_row;
+    int   m_col;
+    int   m_health;
+};
